bad2cab7a635e814d140f72e9db7216b2c068c8a
Sebastian5926 <vargascallaca56@gmail.com>
2026-02-11 19:58:14 +0100

    Eliminar archivos JS separados e integrar en operaciones.js

--- multiplicar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("#btnMultiplicar").click(function () {
-    let n1 = parseFloat($("#num1").val()) || 0;
-    let n2 = parseFloat($("#num2").val()) || 0;
-
-    let multiplicacion = n1 * n2;
-    $("#resultado").val(multiplicacion);
-  });
-});
